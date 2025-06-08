6284689e0a6a38ef00241b2104a1b0f7d8a26d21
VictorBarretoAndrade <163767616+VictorBarretoAndrade@users.noreply.github.com>
2025-06-08 13:40:20 -0300

    Create Remedio.java
    Signed-off-by: VictorBarretoAndrade <163767616+VictorBarretoAndrade@users.noreply.github.com>

---
+++ Remedio.java
@@ -0,0 +1,35 @@
+package entidades;
+
+public class Remedio extends Produto {
+    private String tarja;
+    private String sintomasAparentes;
+
+    public Remedio(double preco, int quantidade, String nome, String validade, String tarja, String sintomas) {
+        super(preco, quantidade, nome, validade);
+        this.tarja = tarja;
+        this.sintomasAparentes = sintomas;
+    }
+
+	public String getTarja() {
+		return tarja;
+	}
+
+	public void setTarja(String tarja) {
+		this.tarja = tarja;
+	}
+
+	public String getSintomasAparentes() {
+		return sintomasAparentes;
+	}
+
+	public void setSintomasAparentes(String sintomasAparentes) {
+		this.sintomasAparentes = sintomasAparentes;
+	}
+
+	@Override
+	public String toString() {
+		return super.toString() + "Remedio [tarja=" + tarja + ", sintomasAparentes=" + sintomasAparentes + "]";
+	}
+
+  
+}
