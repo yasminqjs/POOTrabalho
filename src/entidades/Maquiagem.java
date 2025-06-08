b50594d971d91fbc2ddddba07b1d7dd4a3adc383
VictorBarretoAndrade <163767616+VictorBarretoAndrade@users.noreply.github.com>
2025-06-08 12:32:58 -0300

    Update Maquiagem.java
    Signed-off-by: VictorBarretoAndrade <163767616+VictorBarretoAndrade@users.noreply.github.com>

--- src/entidades/Maquiagem.java
+++ src/entidades/Maquiagem.java
@@ -1,32 +1,20 @@
 package entidades;
 
-public class Maquiagem extends ProdutoBeleza {
+import interfaces.Exibivel;
+
+public class Maquiagem extends ProdutoBeleza implements Exibivel {
     private String cor;
     private String tipoDePele;
 
-    public Maquiagem(double preco, int quantidade, String nome, String validade,String internacional, String luxo, String cor, String tipoDePele) {
+    public Maquiagem(double preco, int quantidade, String nome, String validade, String internacional, String luxo, String cor, String tipoDePele) {
         super(preco, quantidade, nome, validade, internacional, luxo);
         this.cor = cor;
         this.tipoDePele = tipoDePele;
     }
 
-    // Getters e Setters
-
-    public String getCor() {
-        return cor;
+    @Override
+    public void exibirInformacoes() {
+        System.out.printf("Maquiagem: %s | R$%.2f | %d unidades | Cor: %s | Tipo de pele: %s%n", 
+                          getNome(), getPreco(), getQuantidade(), cor, tipoDePele);
     }
-
-    public void setCor(String cor) {
-        this.cor = cor;
-    }
-
-    public String getTipoDePele() {
-        return tipoDePele;
-    }
-
-    public void setTipoDePele(String tipoDePele) {
-        this.tipoDePele = tipoDePele;
-    }
-
-    
 }
