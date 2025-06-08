40d2506943c37d7da5c2c23b0d8be0ae89d1892e
VictorBarretoAndrade <163767616+VictorBarretoAndrade@users.noreply.github.com>
2025-06-08 13:39:46 -0300

    Create ProdutoHigiene.java
    Signed-off-by: VictorBarretoAndrade <163767616+VictorBarretoAndrade@users.noreply.github.com>

---
+++ ProdutoHigiene.java
@@ -0,0 +1,29 @@
+package entidades;
+
+public class ProdutoHigiene extends Produto {
+    private String tipo; 
+
+    public ProdutoHigiene(double preco, int quantidade, String nome, String validade, String tipo) {
+        super(preco, quantidade, nome, validade);
+        this.tipo = tipo;
+    }
+
+    public String getTipo() {
+		return tipo;
+	}
+
+
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	@Override
+	public String toString() {
+		return super.toString() + "ProdutoHigiene [tipo=" + tipo + "]";
+	}
+
+	
+	
+	
+}
