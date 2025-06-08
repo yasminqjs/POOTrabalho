36cff35318246efcb4e2b1fcfe7c72c5cceb95b3
VictorBarretoAndrade <163767616+VictorBarretoAndrade@users.noreply.github.com>
2025-06-08 13:40:49 -0300

    Create ProdutoAlimenticio.java
    Signed-off-by: VictorBarretoAndrade <163767616+VictorBarretoAndrade@users.noreply.github.com>

---
+++ ProdutoAlimenticio.java
@@ -0,0 +1,35 @@
+package entidades;
+
+public abstract class ProdutoAlimenticio extends Produto {
+    private String tipoComida; // comida ou bebida
+    private String ehFit; // "sim" ou "nao"
+
+    public ProdutoAlimenticio(double preco, int quantidade, String nome, String validade, String tipoComida, String ehFit) {
+        super(preco, quantidade, nome, validade);
+        this.tipoComida = tipoComida;
+        this.ehFit = ehFit;
+    }
+
+	public String getTipoComida() {
+		return tipoComida;
+	}
+
+	public void setTipoComida(String tipoComida) {
+		this.tipoComida = tipoComida;
+	}
+
+	public String getEhFit() {
+		return ehFit;
+	}
+
+	public void setEhFit(String ehFit) {
+		this.ehFit = ehFit;
+	}
+
+	@Override
+	public String toString() {
+		return super.toString() + "ProdutoAlimenticio [tipoComida=" + tipoComida + ", ehFit=" + ehFit + "]";
+	}
+
+	
+}
