460a5cdf02a54f8958035a8f675d5f043e44c94a
VictorBarretoAndrade <163767616+VictorBarretoAndrade@users.noreply.github.com>
2025-06-08 13:40:30 -0300

    Create TinturaParaCabelo.java
    Signed-off-by: VictorBarretoAndrade <163767616+VictorBarretoAndrade@users.noreply.github.com>

---
+++ TinturaParaCabelo.java
@@ -0,0 +1,36 @@
+package entidades;
+
+public class TinturaParaCabelo extends ProdutoBeleza {
+    private String tipoDeCabelo;
+    private String cor;
+
+    public TinturaParaCabelo(double preco, int quantidade, String nome, String validade, String internacional, String luxo, String tipoDeCabelo, String cor) {
+        super(preco, quantidade, nome, validade, internacional, luxo);
+        this.tipoDeCabelo = tipoDeCabelo;
+        this.cor = cor;
+    }
+
+	public String getTipoDeCabelo() {
+		return tipoDeCabelo;
+	}
+
+	public void setTipoDeCabelo(String tipoDeCabelo) {
+		this.tipoDeCabelo = tipoDeCabelo;
+	}
+
+	public String getCor() {
+		return cor;
+	}
+
+	public void setCor(String cor) {
+		this.cor = cor;
+	}
+
+	@Override
+	public String toString() {
+		return super.toString() +  "TinturaParaCabelo [tipoDeCabelo=" + tipoDeCabelo + ", cor=" + cor + "]";
+	}
+	
+	
+}
+
